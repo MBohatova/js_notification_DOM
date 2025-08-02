02f057782bf11cd0daf58b6bbae81bfe04ed63e5
Mariia Bohatova <maria.bogatova.01@gmail.com>
2025-08-02 14:09:45 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    let typeClass;
+    if (type === "success") typeClass = "success";
+    if (type === "error") typeClass = "error";
+    if (type === "warning") typeClass = "warning";
+    const uniqueId = `notification-${Date.now()}-${Math.random()}`;
+    document.querySelector("body").insertAdjacentHTML("beforeend", `
+      <div id="${uniqueId}" class="notification ${typeClass}" style="top: ${posTop}px; right: ${posRight}px;">
+        <h2 class="title">${title}</h2>
+        <p>${description}</p>
+      </div>
+    `);
+    const notification = document.getElementById(uniqueId);
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
